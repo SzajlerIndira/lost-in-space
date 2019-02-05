2b9edfb45cae28d577c5662ba9e16394ec75df84
Szajler Indira <szajler.indira@gmail.com>
2019-02-05 10:14:31 +0100

    First commit

---
+++ src/test/java/com/codecool/lostinspace/LostInSpaceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.codecool.lostinspace;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LostInSpaceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+

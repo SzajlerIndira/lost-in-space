8b80930ab8e827adc9975fd60a6a82ad7f9123be
Noemi Szoke <szokenoemijob@gmail.com>
2019-02-05 15:19:20 +0100

    Property and EntityDatabase class and methods implemented

--- src/main/java/com/codecool/lostinspace/entity/Property.java
+++ src/main/java/com/codecool/lostinspace/entity/Property.java
@@ -1,4 +1,22 @@
 package com.codecool.lostinspace.entity;
 
 public class Property {
+    private int id;
+    private static int idCounter = 0;
+    private int price;
+    private int size;
+    private Planet planet;
+
+
+    public Property( int price, int size, Planet planet) {
+        idCounter++;
+        this.id = idCounter;
+        this.price = price;
+        this.size = size;
+        this.planet = planet;
+    }
+
+    public int getSize() {
+        return size;
+    }
 }
--- src/main/java/com/codecool/lostinspace/service/EntityDatabase.java
+++ src/main/java/com/codecool/lostinspace/service/EntityDatabase.java
@@ -1,4 +1,28 @@
 package com.codecool.lostinspace.service;
 
+import com.codecool.lostinspace.entity.Planet;
+import com.codecool.lostinspace.entity.Property;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class EntityDatabase {
+    private List<Property> properties = new ArrayList<>();
+    private List<Planet> planets = new ArrayList<>();
+
+    public List<String> getPlanets() {
+        List<String> planetNames = new ArrayList<>();
+        for (Planet planet : planets) {
+            planetNames.add(planet.getName());
+        }
+        return planetNames;
+    }
+
+    public int getTotalPropertySizePerPlanet(Planet planet){
+        int totalPropertySize = 0;
+        for(Property property:properties){
+            totalPropertySize += property.getSize();
+
+        }
+    }
 }

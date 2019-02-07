b00df5c2d478504954401c10c7dc3b166dcbda89
Szajler Indira <szajler.indira@gmail.com>
2019-02-07 21:13:24 +0100

    Global refactor because EntityDatabase class is became a singleton

--- src/main/java/com/codecool/lostinspace/config/PlanetConfig.java
+++ src/main/java/com/codecool/lostinspace/config/PlanetConfig.java
@@ -1,18 +1,8 @@
 package com.codecool.lostinspace.config;
 
-import com.codecool.lostinspace.service.EntityDatabase;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class PlanetConfig {
-
-
-//        @Bean
-//        public EntityDatabase getPlanets(){
-//            return new EntityDatabase();
-//        }
-
-
 
 }
